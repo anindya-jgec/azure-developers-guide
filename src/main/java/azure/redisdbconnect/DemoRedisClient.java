805951c11ac7ec9ff650cefc5edc6b2c2c6d200e
ANINDYA PAL <anindya.jgec@gmail.com>
2023-02-14 19:33:49 -0600

    Azure Redis Connection !!
    Signed-off-by: Anindya Pal <Anindya.Pal@walgreens.com>

---
+++ src/main/java/azure/redisdbconnect/DemoRedisClient.java
@@ -0,0 +1,40 @@
+package azure.redisdbconnect;
+
+import redis.clients.jedis.DefaultJedisClientConfig;
+import redis.clients.jedis.Jedis;
+
+public class DemoRedisClient {
+
+    private static final String REDISCACHEHOSTNAME = "<REDIS_HOST>";
+    private static final String REDISCACHEKEY = "<REDIS_KEY>";
+    public static void main(String[] a){
+        boolean useSsl = true;
+
+        // Connect to the Azure Cache for Redis over the TLS/SSL port using the key.
+        Jedis jedis = new Jedis(REDISCACHEHOSTNAME, 6380, DefaultJedisClientConfig.builder()
+                .password(REDISCACHEKEY)
+                .ssl(useSsl)
+                .build());
+
+        // Perform cache operations using the cache connection object...
+        System.out.println( "\nCache Command  : Ping" );
+        System.out.println( "Cache Response : " + jedis.ping());
+
+        // Simple get and put of integral data types into the cache
+        System.out.println( "\nCache Command  : GET Message" );
+        System.out.println( "Cache Response : " + jedis.get("Message"));
+
+        System.out.println( "\nCache Command  : SET Message" );
+        System.out.println( "Cache Response : " + jedis.set("Message", "Hello! The cache is working from Java!"));
+
+        // Demonstrate "SET Message" executed as expected...
+        System.out.println( "\nCache Command  : GET Message" );
+        System.out.println( "Cache Response : " + jedis.get("Message"));
+
+        // Get the client list, useful to see if connection list is growing...
+        System.out.println( "\nCache Command  : CLIENT LIST" );
+        System.out.println( "Cache Response : " + jedis.clientList());
+
+        jedis.close();
+    }
+}
