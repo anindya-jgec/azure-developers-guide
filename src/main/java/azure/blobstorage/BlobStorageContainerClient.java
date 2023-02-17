86393a16cca62e14ea7eeb1ccd68487844e36330
Anindya Pal <Anindya.Pal@walgreens.com>
2023-02-17 15:36:12 -0600

    Azure Blob Storage connect !!
    Signed-off-by: Anindya Pal <Anindya.Pal@walgreens.com>

--- src/main/java/azure/blobstorage/BlobStorageContainerClient.java
+++ src/main/java/azure/blobstorage/BlobStorageContainerClient.java
@@ -7,14 +7,18 @@ import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
 import com.azure.storage.blob.models.BlobItem;
 
+import java.util.Map;
+
 /**
+ * https://learn.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-java?tabs=powershell%2Cconnection-string%2Croles-azure-portal%2Csign-in-powershell
+ *
  * This class connects to a Az-Storage account
  * - Creates a cloud blob container.
  * - Lists files present in a container
  * - Upload files in a cloud container.
  */
 public class BlobStorageContainerClient {
-    private final String blobConnectionString = "<AZ_StorageAccount_Connection_String>";
+    private final String blobConnectionString = "DefaultEndpointsProtocol=https;AccountName=azap204storage;AccountKey=Xle21XnFxFBSpWSo/0qrZTjX2CZQaZv2gqd3fb+U7lIJIxM6hJLgDAoLZDDX3VKyX1elDWXx9ayq+AStixgf+g==;EndpointSuffix=core.windows.net";
 
     /**
      * Main method to test..
@@ -46,14 +50,17 @@ public class BlobStorageContainerClient {
 
         // List the blob(s) in the container.
         for (BlobItem blobItem : blobContainerClient.listBlobs()) {
+            //Map<String, String> blobMetaData = blobItem.getMetadata();
             System.out.println("\t" + blobItem.getName());
+            //blobMetaData.keySet().forEach((key) ->
+                    //System.out.println("Key - "+key+ ":: value -"+blobMetaData.get(key)));
         }
 
         // Upload the blob
         String fileName = "/Users/ctsuser1/Desktop/Hiring_Manager_Certificate_02062023.pdf";
         BlobClient blobClient = blobContainerClient.getBlobClient(fileName);
         System.out.println("\nUploading to Blob storage as blob:\n\t" + blobClient.getBlobUrl());
-        blobClient.uploadFromFile(fileName);
+        //blobClient.uploadFromFile(fileName);
         System.out.println("Uploaded file into Container successfully..");
     }
 }
